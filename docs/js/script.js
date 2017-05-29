6eba1fb5f2b900903df76dfece68b1cf3065cbb5
rifutan <ishizaki-shota@KYCNT1703-07.local>
2017-05-29 15:18:34 +0900

    打ち上がるけど爆発しない状態

--- docs/js/script.js
+++ docs/js/script.js
@@ -9835,18 +9835,26 @@ var Fireworks1 = function () {
     this.canvasWidth = this.stage.canvas.width;
     this.canvasHeight = this.stage.canvas.height;
     this.fireworks = [];
+    this.fireworks.explosion = false;
     this.colorList = ["#fff599", "#00ff7f", "#ff69b4", "#99eeff"];
 
     this.background();
     window.setInterval(function () {
-      _this.emitFireworks();
+      _this.startFireworks();
     }, 2000);
 
     createjs.Ticker.addEventListener("tick", function () {
-      _this.updateFireworks();
+      if (_this.fireworks.explosion == false) {
+        _this.launchFireworks();
+      } else {
+        _this.explodeFireworks();
+      }
       _this.stage.update();
     });
   }
+
+  // 背景
+
 
   _createClass(Fireworks1, [{
     key: "background",
@@ -9855,38 +9863,48 @@ var Fireworks1 = function () {
       background.graphics.beginLinearGradientFill(["#000000", "#191970"], [0, 1], this.canvasWidth / 2, 0, this.canvasWidth / 2, this.canvasHeight).drawRect(0, 0, this.canvasWidth, this.canvasHeight);
       this.stage.addChild(background);
     }
+
+    // 1つの花火についての初期設定
+
   }, {
-    key: "emitFireworks",
-    value: function emitFireworks() {
+    key: "startFireworks",
+    value: function startFireworks() {
+      this.sparkPositionX = 100 + Math.random() * (this.canvasWidth - 200);
+      this.sparkPositionY = 100 + Math.random() * (this.canvasHeight - 200);
       var color = this.colorList[Math.floor(Math.random() * this.colorList.length)];
-      var size = 1;
+      var sparkSize = 1;
       var sparkLength = 500;
-      var sparkPositionX = 100 + Math.random() * (this.canvasWidth - 200);
-      var sparkPositionY = 100 + Math.random() * (this.canvasHeight - 200);
       for (var i = 0; i < sparkLength; i++) {
         var spark = new createjs.Shape();
         this.stage.addChild(spark);
-        spark.graphics.beginFill(color).drawCircle(0, 0, size);
-        spark.x = sparkPositionX;
-        spark.y = sparkPositionY;
-        spark.angle = Math.random() * 360;
-        spark.radian = spark.angle * Math.PI / 180;
-        spark.directionX = Math.cos(spark.radian);
-        spark.directionY = Math.sin(spark.radian);
-        if (i % 3 != 0) {
-          spark.vx = (8 + 7 * Math.random()) * spark.directionX;
-          spark.vy = (8 + 7 * Math.random()) * spark.directionY;
-        } else {
-          spark.vx = (1 + 8 * Math.random()) * spark.directionX;
-          spark.vy = (1 + 8 * Math.random()) * spark.directionY;
-        }
-        spark.life = Math.random() * 30 + 30;
+        spark.graphics.beginFill(color).drawCircle(0, 0, sparkSize);
+        spark.x = this.sparkPositionX;
+        spark.y = this.canvasHeight;
         this.fireworks.push(spark);
       }
     }
+
+    // 花火を打ち上げる
+
   }, {
-    key: "updateFireworks",
-    value: function updateFireworks() {
+    key: "launchFireworks",
+    value: function launchFireworks() {
+      for (var i = 0; i < this.fireworks.length; i++) {
+        var spark = this.fireworks[i];
+        spark.vy = -15;
+        spark.y += spark.vy;
+        if (spark.y < this.sparkPositionY) {
+          this.fireworks.explosion = true;
+          createjs.Ticker._timerId = null;
+        }
+      }
+    }
+
+    // 花火を爆発させる
+
+  }, {
+    key: "explodeFireworks",
+    value: function explodeFireworks() {
       for (var i = 0; i < this.fireworks.length; i++) {
         var spark = this.fireworks[i];
         spark.vy += 0.2;
@@ -9894,6 +9912,7 @@ var Fireworks1 = function () {
         spark.vy *= 0.9;
         spark.x += spark.vx;
         spark.y += spark.vy;
+        console.log(spark.y);
         spark.life -= 1;
         if (spark.life < 20) {
           spark.alpha = spark.life / 20;
@@ -9905,9 +9924,33 @@ var Fireworks1 = function () {
         }
       }
     }
-  }, {
-    key: "setLife",
-    value: function setLife() {}
+
+    // emitFireworks() {
+    //   const color = this.colorList[Math.floor(Math.random() * this.colorList.length)];
+    //   const size = 1;
+    //   const sparkLength = 500;
+    //   for (let i = 0; i < sparkLength; i++) {
+    //     const spark = new createjs.Shape();
+    //     this.stage.addChild(spark);
+    //     spark.graphics.beginFill(color).drawCircle(0, 0, size);
+    //     spark.x = this.sparkPositionX;
+    //     spark.y = this.sparkPositionY;
+    //     spark.angle = Math.random() * 360;
+    //     spark.radian = spark.angle * Math.PI / 180;
+    //     spark.directionX = Math.cos(spark.radian);
+    //     spark.directionY = Math.sin(spark.radian);
+    //     if (i % 3 != 0) {
+    //       spark.vx = (8 + 7 * Math.random()) * spark.directionX;
+    //       spark.vy = (8 + 7 * Math.random()) * spark.directionY;
+    //     } else {
+    //       spark.vx = (1 + 8 * Math.random()) * spark.directionX;
+    //       spark.vy = (1 + 8 * Math.random()) * spark.directionY;
+    //     }
+    //     spark.life = Math.random() * 30 + 30;
+    //     this.fireworks.push(spark);
+    //   }
+    // }
+
   }]);
 
   return Fireworks1;
--- src/js/lib/fireworks1.js
+++ src/js/lib/fireworks1.js
@@ -4,19 +4,25 @@ export default class Fireworks1 {
     this.canvasWidth = this.stage.canvas.width;
     this.canvasHeight = this.stage.canvas.height;
     this.fireworks = [];
+    this.fireworks.explosion = false;
     this.colorList = ["#fff599", "#00ff7f", "#ff69b4", "#99eeff"];
 
     this.background();
     window.setInterval(() => {
-      this.emitFireworks()
+      this.startFireworks()
     }, 2000);
 
     createjs.Ticker.addEventListener("tick", () => {
-      this.updateFireworks();
+      if (this.fireworks.explosion == false) {
+        this.launchFireworks();
+      } else {
+        this.explodeFireworks();
+      }
       this.stage.update();
     });
   }
 
+  // 背景
   background() {
     const background = new createjs.Shape();
     background.graphics.beginLinearGradientFill(["#000000", "#191970"], [0, 1], this.canvasWidth/2, 0, this.canvasWidth/2, this.canvasHeight)
@@ -24,34 +30,38 @@ export default class Fireworks1 {
     this.stage.addChild(background);
   }
 
-  emitFireworks() {
+  // 1つの花火についての初期設定
+  startFireworks() {
+    this.sparkPositionX = 100 + Math.random() * (this.canvasWidth - 200);
+    this.sparkPositionY = 100 + Math.random() * (this.canvasHeight - 200);
     const color = this.colorList[Math.floor(Math.random() * this.colorList.length)];
-    const size = 1;
+    const sparkSize = 1;
     const sparkLength = 500;
-    const sparkPositionX = 100 + Math.random() * (this.canvasWidth - 200);
-    const sparkPositionY = 100 + Math.random() * (this.canvasHeight - 200);
     for (let i = 0; i < sparkLength; i++) {
       const spark = new createjs.Shape();
       this.stage.addChild(spark);
-      spark.graphics.beginFill(color).drawCircle(0, 0, size);
-      spark.x = sparkPositionX;
-      spark.y = sparkPositionY;
-      spark.angle = Math.random() * 360;
-      spark.radian = spark.angle * Math.PI / 180;
-      spark.directionX = Math.cos(spark.radian);
-      spark.directionY = Math.sin(spark.radian);
-      if (i % 3 != 0) {
-        spark.vx = (8 + 7 * Math.random()) * spark.directionX;
-        spark.vy = (8 + 7 * Math.random()) * spark.directionY;
-      } else {
-        spark.vx = (1 + 8 * Math.random()) * spark.directionX;
-        spark.vy = (1 + 8 * Math.random()) * spark.directionY;
-      }
-      spark.life = Math.random() * 30 + 30;
+      spark.graphics.beginFill(color).drawCircle(0, 0, sparkSize);
+      spark.x = this.sparkPositionX;
+      spark.y = this.canvasHeight;
       this.fireworks.push(spark);
     }
   }
-  updateFireworks() {
+
+  // 花火を打ち上げる
+  launchFireworks() {
+    for (let i = 0; i < this.fireworks.length; i++) {
+      const spark = this.fireworks[i];
+      spark.vy = -15;
+      spark.y += spark.vy;
+      if (spark.y < this.sparkPositionY) {
+        this.fireworks.explosion = true;
+        createjs.Ticker._timerId = null;
+      }
+    }
+  }
+
+  // 花火を爆発させる
+  explodeFireworks() {
     for (let i = 0; i < this.fireworks.length; i++) {
       const spark = this.fireworks[i];
       spark.vy += 0.2;
@@ -59,6 +69,7 @@ export default class Fireworks1 {
       spark.vy *= 0.9;
       spark.x += spark.vx;
       spark.y += spark.vy;
+      console.log(spark.y);
       spark.life -= 1;
       if (spark.life < 20) {
         spark.alpha = spark.life / 20;
@@ -70,7 +81,30 @@ export default class Fireworks1 {
       }
     }
   }
-  setLife() {
 
-  }
+  // emitFireworks() {
+  //   const color = this.colorList[Math.floor(Math.random() * this.colorList.length)];
+  //   const size = 1;
+  //   const sparkLength = 500;
+  //   for (let i = 0; i < sparkLength; i++) {
+  //     const spark = new createjs.Shape();
+  //     this.stage.addChild(spark);
+  //     spark.graphics.beginFill(color).drawCircle(0, 0, size);
+  //     spark.x = this.sparkPositionX;
+  //     spark.y = this.sparkPositionY;
+  //     spark.angle = Math.random() * 360;
+  //     spark.radian = spark.angle * Math.PI / 180;
+  //     spark.directionX = Math.cos(spark.radian);
+  //     spark.directionY = Math.sin(spark.radian);
+  //     if (i % 3 != 0) {
+  //       spark.vx = (8 + 7 * Math.random()) * spark.directionX;
+  //       spark.vy = (8 + 7 * Math.random()) * spark.directionY;
+  //     } else {
+  //       spark.vx = (1 + 8 * Math.random()) * spark.directionX;
+  //       spark.vy = (1 + 8 * Math.random()) * spark.directionY;
+  //     }
+  //     spark.life = Math.random() * 30 + 30;
+  //     this.fireworks.push(spark);
+  //   }
+  // }
 }
